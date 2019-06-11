faf5619c56a32aeb8d808f206ec125d5ba5b905d
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-10 21:10:23 -0700

    swagger work

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,8 +4,29 @@ const express = require('express');
 const router = express.Router();
 const auth = require ('../auth/middleware');
 
+
+/**
+ * Get a list of books for model provided
+ * @route GET /{books}
+ * @param {string} books.path.required - Resource model name
+ * @returns {Object} 500 - Server error
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
+ */
+
 router.get('/books', auth, handleGetAll);
+
+/**
+ * Get a list of records for book id provided
+ * @route GET /{books}/{id}
+ * @param {string} book.path.required - Resource model name
+ * @param {number} id.path.required - Resource model name
+ * @returns {Object} 500 - Server error
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
+ */
+
 router.get('/books/:id',auth, handleGetOne);
+
+
 
 // Route Handlers
 function handleGetAll(req, res, next) {
