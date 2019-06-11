43d76a1c137162bc9385bc50e31be8f8b7ed02ae
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-10 21:12:41 -0700

    more swagger

--- src/auth/router.js
+++ src/auth/router.js
@@ -7,6 +7,14 @@ const authRouter = express.Router();
 const User = require('./users-model.js');
 const auth = require('./middleware.js');
 
+/**
+ * Signup
+ * @route GET /{signup}
+ * @param {string} signup.path.required - Resource model name
+ * @returns {Object} 500 - Server error
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
+ */
+
 authRouter.post('/signup', (req, res, next) => {
   console.log(req.body);
   let user = new User(req.body);
@@ -19,6 +27,14 @@ authRouter.post('/signup', (req, res, next) => {
       res.send(req.token);
     }).catch(next);
 });
+
+/**
+ * Signin
+ * @route GET /{signin}
+ * @param {string} signin.path.required - Resource model name
+ * @returns {Object} 500 - Server error
+ * @returns {Object} 200 - { count: 2, results: [{}, {}]}
+ */
 
 authRouter.get('/signin', auth, (req, res) => {
   res.cookie('auth', req.token);
