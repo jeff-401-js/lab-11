a52bcda9a51fa5328251ab69f66543c878634bea
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-10 21:19:20 -0700

    jsdoc work

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,6 +4,11 @@ const express = require('express');
 const router = express.Router();
 const auth = require ('../auth/middleware');
 
+/**
+ * Router Module (books)
+ * Integrates with various models through a common Interface (.get())
+ * @module src/routes/books
+ */
 
 /**
  * Get a list of books for model provided
@@ -27,9 +32,16 @@ router.get('/books', auth, handleGetAll);
 router.get('/books/:id',auth, handleGetOne);
 
 
+/**
+   * @function handleGetAll
+   * @param {object} request - request object
+   * @param {object} response - response object
+   * @param {function} next - calls next middleware
+   * @desc Middleware that handles get all route call
+   */
 
 // Route Handlers
-function handleGetAll(req, res, next) {
+function handleGetAll(req, res) {
   let books = {
     count: 3,
     results: [
@@ -41,11 +53,24 @@ function handleGetAll(req, res, next) {
   res.status(200).json(books);
 }
 
-function handleGetOne(req, res, next) {
+/**
+   * @function handleGetOne
+   * @param {object} request - request object
+   * @param {object} response - response object
+   * @param {function} next - calls next middleware
+   * @desc Middleware that handles get one call
+   */
+
+function handleGetOne(req, res) {
   let book = {
     title:'Moby Dick',
   };
   res.status(200).json(book);
 }
+
+/**
+* Export object with app and start methods attached
+* @type {Object}
+ */
 
 module.exports = router;
